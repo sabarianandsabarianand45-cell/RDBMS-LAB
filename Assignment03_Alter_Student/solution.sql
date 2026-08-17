454ecafd989de52474a8e5d5d6bd39243b42c1dd
sabarianandsabarianand45-cell <sabarianandsabarianand45@gmail.com>
2026-08-17 12:51:05 +0530

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,19 +1,3 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5)
-);
-
--- Alter Student table
-
--- Add Email
-
--- Add PhoneNumber
-
--- Display structure
+USE COLLAGEDBBCA;
+alter TABLE STUDENT add (Email VARCHAR(30),PHONENUMBER INT (10));
+DESC STUDENT;
